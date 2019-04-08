5e7ae18587ae7742d339484a8514ecbfaf5d2917
Emrah Sönmez <emrahsonm3z@gmail.com>
2019-04-08 15:23:23 +0300

    added effectScatter. But not yet complete.

--- src/routes/ElectionResultsMap/ElectionResultsMap.tsx
+++ src/routes/ElectionResultsMap/ElectionResultsMap.tsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import ReactEcharts from 'echarts-for-react';
-import echarts from 'echarts';
+import ReactEchartsCore from 'echarts-for-react/lib/core';
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/effectScatter';
+import 'echarts/lib/chart/map';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/geo';
+import 'echarts/lib/component/title';
+import 'echarts/lib/component/toolbox';
 
 import { getElectionResults, getPartyIcon } from './func';
 import { CalculatedElectionResult } from './types';
@@ -12,6 +18,14 @@ const data = getElectionResults();
 type ElectionResultsMapState = {
   option: any;
   data: any;
+};
+
+const geoCoordMap: any = {
+  Manisa: [28.158319, 38.494787],
+  Konya: [33.066589, 39.103627],
+  Ankara: [32.055303, 39.818594],
+  Eskişehir: [31.502003, 39.453424],
+  Çankırı: [33.33271, 40.462331],
 };
 
 const titleCase = (str: string): string => {
@@ -34,12 +48,30 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
 
   getInitialState = () => ({ option: this.getOption(data), data: data });
 
+  // Büyük şehir olanların belirlenmesi
+  convertData = (data: Array<CalculatedElectionResult>) => {
+    let scatterData = [];
+
+    const metropolitanDatas = data.filter(d => d.isMetropolitan === true);
+    for (var i = 0; i < metropolitanDatas.length; i++) {
+      var geoCoord = geoCoordMap[metropolitanDatas[i].name];
+      if (geoCoord) {
+        scatterData.push({
+          name: metropolitanDatas[i].name,
+          value: geoCoord.concat(parseInt(metropolitanDatas[i].winner.voteCount)),
+          color: metropolitanDatas[i].winner.color,
+        });
+      }
+    }
+    return scatterData;
+  };
+
   getOption = (data: any): any => {
     return {
       backgroundColor: '#404a59',
       title: {
-        text: 'Türkiye Seçim Sonuçları',
-        subtext: 'Yerel Seçimler',
+        text: '2019 Türkiye Seçim Sonuçları',
+        subtext: 'İl Bazlı Yerel Seçim Sonuçları',
         left: 'center',
         top: 10,
         x: 'center',
@@ -51,6 +83,7 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
         },
       },
       tooltip: {
+        id: 1,
         trigger: 'item',
         backgroundColor: 'transparent',
         formatter: function(params: any) {
@@ -92,7 +125,7 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
       geo: {
         map: 'turkey',
         right: '10',
-        top: '5%',
+        top: 50,
         left: '10',
         zoom: 0.8,
         label: {
@@ -117,7 +150,7 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
                 },
                 {
                   offset: 1,
-                  color: 'rgba(	47,79,79, .2)', // 100%
+                  color: 'rgba(47, 79, 79, .2)', // 100%
                 },
               ],
               globalCoord: false, //
@@ -128,12 +161,58 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
             shadowBlur: 12,
           },
           emphasis: {
-            areaColor: '#FFF',
+            areaColor: '#404a59',
             borderWidth: 0,
           },
         },
       },
       series: [
+        {
+          type: 'effectScatter',
+          coordinateSystem: 'geo',
+          data: this.convertData(data),
+          symbolSize: function(val: any) {
+            return window.innerWidth / 150 + 5;
+          },
+          symbolOffset: ['50%', '50%'],
+          showEffectOn: 'render',
+          rippleEffect: {
+            period: 10,
+            scale: 3,
+            brushType: 'fill',
+          },
+          hoverAnimation: true,
+          itemStyle: {
+            normal: {
+              // color: function(params: any) {
+              //   // return params.data.color;
+              //   return '#fff';
+              // },
+              color: {
+                type: 'linear',
+                x: 0,
+                y: 0,
+                x2: 0,
+                y2: 1,
+                colorStops: [
+                  {
+                    offset: 0,
+                    color: 'red', // color at 0% position
+                  },
+                  {
+                    offset: 1,
+                    color: 'blue', // color at 100% position
+                  },
+                ],
+                global: false, // false by default
+              },
+            },
+          },
+          zlevel: 1,
+          tooltip: {
+            show: false,
+          },
+        },
         {
           type: 'map',
           geoIndex: 0,
@@ -141,7 +220,7 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
             normal: {
               borderColor: 'white',
               color: function(obj: any) {
-                if (typeof obj.data === 'undefined') return '#000';
+                if (typeof obj.data === 'undefined') return '#fff';
                 var color = obj.data.winner.color;
 
                 return color;
@@ -157,7 +236,8 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
   render() {
     return (
       <div className="parent">
-        <ReactEcharts
+        <ReactEchartsCore
+          echarts={echarts}
           option={this.state.option}
           style={{ height: '100vh', width: '100vw' }}
           className="react_for_echarts"
