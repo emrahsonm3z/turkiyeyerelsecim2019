a99a9de8a8c531c4578c62f15ca1f3efb14ecd07
Emrah Sönmez <emrahsonm3z@gmail.com>
2019-04-08 15:49:57 +0300

    Reduce bundle and change map title.

--- src/routes/ElectionResultsMap/ElectionResultsMap.tsx
+++ src/routes/ElectionResultsMap/ElectionResultsMap.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactEchartsCore from 'echarts-for-react/lib/core';
 import echarts from 'echarts/lib/echarts';
-import 'echarts/lib/chart/effectScatter';
 import 'echarts/lib/chart/map';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/geo';
@@ -9,7 +8,6 @@ import 'echarts/lib/component/title';
 import 'echarts/lib/component/toolbox';
 
 import { getElectionResults, getPartyIcon } from './func';
-import { CalculatedElectionResult } from './types';
 
 import turkeyGeoMapData from './data/tr.json';
 
@@ -18,14 +16,6 @@ const data = getElectionResults();
 type ElectionResultsMapState = {
   option: any;
   data: any;
-};
-
-const geoCoordMap: any = {
-  Manisa: [28.158319, 38.494787],
-  Konya: [33.066589, 39.103627],
-  Ankara: [32.055303, 39.818594],
-  Eskişehir: [31.502003, 39.453424],
-  Çankırı: [33.33271, 40.462331],
 };
 
 const titleCase = (str: string): string => {
@@ -48,24 +38,6 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
 
   getInitialState = () => ({ option: this.getOption(data), data: data });
 
-  // Büyük şehir olanların belirlenmesi
-  convertData = (data: Array<CalculatedElectionResult>) => {
-    let scatterData = [];
-
-    const metropolitanDatas = data.filter(d => d.isMetropolitan === true);
-    for (var i = 0; i < metropolitanDatas.length; i++) {
-      var geoCoord = geoCoordMap[metropolitanDatas[i].name];
-      if (geoCoord) {
-        scatterData.push({
-          name: metropolitanDatas[i].name,
-          value: geoCoord.concat(parseInt(metropolitanDatas[i].winner.voteCount)),
-          color: metropolitanDatas[i].winner.color,
-        });
-      }
-    }
-    return scatterData;
-  };
-
   getOption = (data: any): any => {
     return {
       backgroundColor: '#404a59',
@@ -83,7 +55,6 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
         },
       },
       tooltip: {
-        id: 1,
         trigger: 'item',
         backgroundColor: 'transparent',
         formatter: function(params: any) {
@@ -161,58 +132,12 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
             shadowBlur: 12,
           },
           emphasis: {
-            areaColor: '#404a59',
+            areaColor: '#FFF',
             borderWidth: 0,
           },
         },
       },
       series: [
-        {
-          type: 'effectScatter',
-          coordinateSystem: 'geo',
-          data: this.convertData(data),
-          symbolSize: function(val: any) {
-            return window.innerWidth / 150 + 5;
-          },
-          symbolOffset: ['50%', '50%'],
-          showEffectOn: 'render',
-          rippleEffect: {
-            period: 10,
-            scale: 3,
-            brushType: 'fill',
-          },
-          hoverAnimation: true,
-          itemStyle: {
-            normal: {
-              // color: function(params: any) {
-              //   // return params.data.color;
-              //   return '#fff';
-              // },
-              color: {
-                type: 'linear',
-                x: 0,
-                y: 0,
-                x2: 0,
-                y2: 1,
-                colorStops: [
-                  {
-                    offset: 0,
-                    color: 'red', // color at 0% position
-                  },
-                  {
-                    offset: 1,
-                    color: 'blue', // color at 100% position
-                  },
-                ],
-                global: false, // false by default
-              },
-            },
-          },
-          zlevel: 1,
-          tooltip: {
-            show: false,
-          },
-        },
         {
           type: 'map',
           geoIndex: 0,
@@ -220,7 +145,7 @@ class ElectionResultsMap extends Component<{}, ElectionResultsMapState> {
             normal: {
               borderColor: 'white',
               color: function(obj: any) {
-                if (typeof obj.data === 'undefined') return '#fff';
+                if (typeof obj.data === 'undefined') return '#000';
                 var color = obj.data.winner.color;
 
                 return color;
